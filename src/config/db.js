8eb97b237ce37e12f567b55c128725635d045d8b
duvindu0905 <duvindudushan@gmail.com>
2024-11-28 23:22:27 +0530

    Set up CI/CD for Cloud Run

--- src/config/db.js
+++ src/config/db.js
@@ -1,14 +1,18 @@
+require('dotenv').config(); // Load .env file locally
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        const mongoURI = process.env.MONGO_URI;
+        await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
         console.log('MongoDB connected...');
     } catch (err) {
-        console.error(err.message);
+        console.error('MongoDB connection error:', err.message);
         process.exit(1); // Exit process with failure
     }
 };
+
 module.exports = connectDB;
