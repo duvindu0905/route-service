4cc91f1612e529a9b09906c00c2b2ef6f2c63d4d
duvindu0905 <duvindudushan@gmail.com>
2024-12-26 23:45:10 +0530

    update swagger

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ nibm index - YR3COBSCCOMP232P-007
 
 Api for route service 
 
-url: https://api.routeservice.bussewaya.online
+url: https://route-service-193893744076.us-central1.run.app/route-service/routes
 
-example: https://api.routeservice.bussewaya.online/route-service/routes
-
-swagger url: https://api.routeservice.bussewaya.online/api-docs/#/
+swagger url: https://route-service-193893744076.us-central1.run.app/api-docs/#/
--- src/controllers/sampleController.js
+++ src/controllers/sampleController.js
@@ -14,9 +14,6 @@ exports.getAllRoutes = async (req, res) => {
 // Get a route by routeNumber
 exports.getRouteByNumber = async (req, res) => {
   try {
-    console.log(`abcetjjiguguyf,${req.params.routeNumber}`)
-    console.log(`abcetjjiguguyf,${req.params.routeNumber}`)
-    console.log(`abcetjjiguguyf,${req.params.routeNumber}`)
     
     const route = await Route.findOne({ routeNumber: req.params.routeNumber }).select('-_id -__v'); // Exclude `_id` and `__v`
     if (!route) {
