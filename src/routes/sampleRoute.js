bdd92ee6cec1051227cd62420af1abb4a2d4b41e
duvindu0905 <duvindudushan@gmail.com>
2024-12-29 05:27:17 +0530

    remove secret

--- src/routes/sampleRoute.js
+++ src/routes/sampleRoute.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const authorizeAdmin = require('../middleware/authorizeAdmin'); // Correct path
-const { getAllRoutes, getRouteByNumber, createRoute, deleteRouteByRouteNumber } = require('../controllers/sampleController');
+const {
+  getAllRoutes,
+  getRouteByNumber,
+  getRouteByRouteId,
+  createRoute,
+  deleteRouteByRouteNumber
+} = require('../controllers/sampleController');
 
 const router = express.Router();
 
-// Use the middleware for the routes that require admin authorization
-router.get('/routes', authorizeAdmin, getAllRoutes);  // Get all routes (requires admin)
-router.get('/routes/:routeNumber', authorizeAdmin, getRouteByNumber); // Get route by routeNumber (requires admin)
-router.post('/routes', authorizeAdmin, createRoute); // Create a new route (requires admin)
-router.delete('/routes/:routeNumber', authorizeAdmin, deleteRouteByRouteNumber); // Delete a route (requires admin)
+// Define routes
+router.get('/routes', getAllRoutes); // Get all routes
+router.get('/routes/:routeNumber', getRouteByNumber); // Get route by routeNumber
+router.post('/routes', createRoute); // Create a new route
+router.delete('/routes/:routeNumber', deleteRouteByRouteNumber);
 
 module.exports = router;
-
-
